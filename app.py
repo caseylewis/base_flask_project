d7a56102e63e2493ffbe30d082113fd71b44cb80
Casey <caseylewis54@yahoo.com>
2022-06-25 19:36:37 -0500

    Updating for the pages to have an example of ajax.

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template, request  # , redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify  # , redirect, url_for
 
 # FLASK APP
 app = Flask(__name__)
+
+user_list_data = [
+    {
+        'name': 'Casey',
+        'phone_number': '817-253-8694',
+    },
+    {
+        'name': 'Natalie',
+        'phone_number': '111-222-3333',
+    }
+]
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -9,14 +20,26 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/first_page')
-def first_page():
-    return render_template('first_page.html')
+@app.route('/javascript_example')
+def javascript_example():
+    return render_template('javascript_example.html')
 
 
-@app.route('/second_page')
-def second_page():
-    return render_template('second_page.html')
+@app.route('/ajax_input_example', methods=['GET', 'POST'])
+def ajax_input_example():
+    return render_template('ajax_input_example.html', user_list=user_list_data)
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    # CREATE NEW USER
+    user = {}
+    # COPY USER FROM FORM VALUES
+    for key, value in request.form.items():
+        user[key] = value
+
+    user_list_data.append(user)
+    return jsonify(user_list_data)
 
 
 if __name__ == '__main__':
